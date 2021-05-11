fab94ef3716cd0c92d15f4beaeca2b8e2d857136
Saurav Dutta <saurav@piktorlabs.com>
2021-05-11 11:59:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+/**
+ * import modules
+ */
+const express = require("express");
+const cors = require("cors");
+const accountSid = "AC9bd28c37ffc54f1cc5fda32bbfcc6bfb";
+const authToken = "445011c25050f7754590ea1396ca32f9";
+const client = require("twilio")(accountSid, authToken);
+
+/**
+ * app config
+ */
+const app = express();
+const port = process.env.PORT || 9000;
+
+/**
+ * middleware
+ */
+app.use(cors());
+app.use(express.json());
+
+/**
+ * function to check slot availability
+ */
+isslotavailable = (data) => {
+    let slotsArray = [];
+
+    for(let i=0; i<data.length; i++) {
+        if(data[i].sessions) {
+            for(let j=0; j<data[i].sessions.length; j++) {
+                if(data[i].sessions[j].min_age_limit == 18 && data[i].sessions[j].available_capacity > 0) {
+                    let obj = {
+                        address: data[i].address,
+                        block_name: data[i].block_name,
+                        slots: data[i].sessions[j].slots
+                    }
+                    slotsArray.push(obj)
+                }
+            }
+        }
+    }
+
+    return slotsArray;
+}
+
+/**
+ * api routes
+ */
+app.post('/findslots', (req, res) => {
+    let result = isslotavailable(req.body.data)
+
+    if(result.length == 0) {
+        result = 'No slots available today'
+    }
+    let dataToArray = JSON.stringify(result);
+    dataToArray = result.split(',').map(item => item.trim());
+    dataToArray = dataToArray.join("\n");
+
+    client.messages
+        .create({
+            body: dataToArray,
+            from: 'whatsapp:+14155238886',
+            to: `whatsapp:+91${req.body.phonenumber}`
+       }) 
+      .then(message => console.log(message.sid))
+      .done();
+ 
+})
+
+/**
+ * listen to port
+ */
+app.listen(port, () => console.log(`Listening on localhost: ${port}`));
